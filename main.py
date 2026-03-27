f9838a7346a82fab6ab9ed16fbb58e41cbed0a9a
David Ragazzi <david.ragazzi@gmail.com>
2026-03-27 07:56:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
+from ulauncher.api.client.Extension import Extension
+from ulauncher.api.client.EventListener import EventListener
+from ulauncher.api.shared.event import KeywordQueryEvent
 import subprocess
 
-def run_query(query):
-    try:
-        subprocess.run(query, shell=True)
-    except:
-        result = subprocess.check_output(
-            ["openclaw", "run", query],
-            text=True
-        )
-        subprocess.run(result, shell=True)
+class AIFallbackExtension(Extension):
+    def __init__(self):
+        super(AIFallbackExtension)
+        self.subscribe(KeywordQueryEvent, QueryHandler())
+
+class QueryHandler(EventListener):
+    def on_event(self, event, extension):
+        query = event.get_argument()
+
+        if not query:
+            return
+
+        try:
+            subprocess.Popen(query, shell=True)
+        except:
+            cmd = extension.preferences.get("openclaw_cmd")
+            result = subprocess.check_output(
+                f"{cmd} \"{query}\"",
+                shell=True,
+                text=True
+            )
+            subprocess.Popen(result, shell=True)
+
+        return None
+
+if __name__ == '__main__':
+    AIFallbackExtension().run()
